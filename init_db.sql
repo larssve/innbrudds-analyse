17b25273359334f4508431eac28f91b981cec61d
larssve <sveen@protonmail.ch>
2025-08-27 22:28:38 +0200

    Add sql to load data

---
+++ init_db.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE TABLE innbruddstips AS
+SELECT mottatt_tid,
+			 CASE
+					WHEN innbruddsdato IS NOT NULL THEN innbruddsdato
+			 		ELSE DATE(mottatt_tid)
+			 END AS innbruddsdato,
+			 LOWER(stedsnavn)
+FROM (
+SELECT DISTINCT(*)
+FROM READ_CSV('innbruddstips-eksempel.csv',
+							dateformat = '%Y-%m-%d',
+							columns = {'mottatt_tid': 'TIMESTAMPTZ', 'innbruddsdato': 'DATE', 'stedsnavn': 'VARCHAR'},
+							-- krav om å hoppe over feil i datoformat
+							store_rejects = true
+							));
