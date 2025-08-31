c5ca23d852605f1d5f6075922594463ef8dc4868
larssve <sveen@protonmail.ch>
2025-08-31 13:53:13 +0200

    Add test for duplicates

--- test/test.py
+++ test/test.py
@@ -11,7 +11,7 @@ class TestRequirements(unittest.TestCase):
         Tests that the date format is formatted YYYY-MM-DD
         """
         test_file = "test/feil-format.csv"
-        test_data = [("01-05-2025","DD-MM-YYYY"), 
+        test_data = [("01-05-2025","DD-MM-YYYY"),
                      ("2025-13-01","YYYY-DD-MM"),
                      ("13-05-2025","MM-DD-YYYY"),
                      ("2025/01/05","YYYY/MM/DD")]
@@ -22,7 +22,7 @@ class TestRequirements(unittest.TestCase):
             for test_date, test_format in test_data:
                 with open(test_file, "w") as csvfile:
                     w = csv.writer(csvfile)
-                    mottatt_tid = "2025-01-05T10:15:08Z" 
+                    mottatt_tid = "2025-01-05T10:15:08Z"
                     sted = "Bislett"
                     w.writerow(["mottatt_tid", "innbruddsdato", "stedsnavn"])
                     w.writerow([mottatt_tid,"2025-01-05",sted])
@@ -41,3 +41,26 @@ class TestRequirements(unittest.TestCase):
                         self.assertEqual(errors, expected_errors)
 
         os.remove(test_file)
+
+    def test_duplicates(self):
+        """
+        Tests that duplicate rows are only included once
+        """
+        test_file = "test/test-duplicates.csv"
+
+        with open(INIT_DB, "r") as sql_file:
+            sql = sql_file.read()
+
+            with open(test_file, "w") as csvfile:
+                w = csv.writer(csvfile)
+                w.writerow(["mottatt_tid", "innbruddsdato", "stedsnavn"])
+                w.writerow(["2025-04-04T21:10:00Z","2025-04-04","Bislett"])
+                w.writerow(["2025-04-04T21:10:00Z","2025-04-04","Bislett"])
+
+            with duckdb.connect() as con:
+                con.execute(sql, {"csv_file": test_file})
+
+                actual, = con.sql("SELECT COUNT(*) FROM innbruddstips").fetchone()
+                expected = 1
+                self.assertEqual(actual, expected)
+
